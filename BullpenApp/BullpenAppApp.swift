c265b80bad3f730814baf138adbf5421915d0478
ben petre <benpetre@gmail.com>
2025-12-02 21:33:02 -0500

    Initial Commit

---
+++ BullpenApp/BullpenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullpenAppApp.swift
+//  BullpenApp
+//
+//  Created by benjamin petre on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BullpenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
